869646203f776e70b52b45cc0d389b16b061ed1e
BryanBlanco10 <bryanbarca20@gmail.com>
2021-08-22 01:28:39 -0500

    Cambios en package.json

--- app/API-v1/middlewares/index.js
+++ app/API-v1/middlewares/index.js
@@ -2,12 +2,10 @@ import { validatorMiddleware } from "./validatorMiddleware";
 import { authMiddleware } from "./authMiddleware";
 import { errorMiddleware } from "./errorMiddleware";
 import { notFoundMiddleware } from "./notFoundMiddleware"
-import { authPublicMiddleware } from "./authPublicMiddleware";
 
 export {
     validatorMiddleware,
     authMiddleware,
     errorMiddleware,
-    notFoundMiddleware,
-    authPublicMiddleware 
+    notFoundMiddleware
 }
