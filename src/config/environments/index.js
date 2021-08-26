41606b8714ea9fc3af27f490557a3a5d419e454d
BryanBlanco10 <bryanbarca20@gmail.com>
2021-08-25 22:48:33 -0500

    Cambios en enviroments.js

--- src/config/environments/index.js
+++ src/config/environments/index.js
@@ -5,8 +5,12 @@ import production from "./production";
 dotenv.config();
 
 const { NODE_ENV } = process.env;
-const currentEnv = development;
 
-if(NODE_ENV == "PRODUCTION") currentEnv == production;
+let currentEnv = development;
+
+if(NODE_ENV == "PRODUCTION") {
+  currentEnv = production
+};
+
 
 export { currentEnv };
